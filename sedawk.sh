0c6bed84487044115acf136308d9dda3b52973ad
Aleksey Pivchenkov <aleksey_pivchenkov@epam.com>
2022-07-08 13:42:53 +0000

    Ready

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,74 @@
 #!/bin/bash
 
 # Put you code here
+
+FILE=./passwd_new
+TARGET_USER_AWK="saned"
+
+# BEGIN preparing the target file
+if [[ -f "$FILE" ]]; then
+  echo "$FILE exists" > /dev/null
+else
+  sudo cp ./passwd ./passwd_new && echo "$FILE created successfully" > /dev/null
+fi
+# END preparing the target file
+
+
+# BEGIN replacing shell using awk
+source_string=$(cat ./passwd_new | grep $TARGET_USER_AWK)
+replacement_string=$(cat ./passwd_new | grep $TARGET_USER_AWK | awk -F':' '{printf $1":"$2":"$3":"$4":"$5":"$6":/bin/bash"}')
+awk -i inplace  -v cuv1="$source_string" -v cuv2="$replacement_string" '{gsub(cuv1,cuv2); print;}' "$FILE"
+# END replacing shell using awk
+
+
+# BEGIN replacing shell using sed
+while IFS= read -r line
+do
+  source_string_sed=$(echo $line | sed "s/:/\n/g")
+  readarray -t s_arr <<< $source_string_sed
+  if [[ ${s_arr[0]} == "avahi" ]]; then
+    line=${s_arr[0]}":"${s_arr[1]}":"${s_arr[2]}":"${s_arr[3]}":"${s_arr[4]}":"${s_arr[5]}":/bin/bash"
+  fi
+  echo $line >> ./passwd_new.tmp
+done < "$FILE"
+mv ./passwd_new.tmp ./passwd_new
+# END replacing shell using sed
+
+
+# BEGIN saving the chosen columns
+while IFS= read -r line
+do
+  source_string_sed=$(echo $line | sed "s/:/\n/g")
+  readarray -t s_arr <<< $source_string_sed
+  line=${s_arr[0]}":"${s_arr[2]}":"${s_arr[4]}":"${s_arr[6]}
+  echo $line >> ./passwd_new.tmp
+done < "$FILE"
+mv ./passwd_new.tmp ./passwd_new
+# END saving the chosen columns
+
+
+# BEGIN removing lines
+while IFS= read -r line
+do
+  if [[ $line == *"daemon"* ]]; then
+    continue
+    #line=""
+  fi
+  echo $line >> ./passwd_new.tmp
+done < "$FILE"
+mv ./passwd_new.tmp ./passwd_new
+# END removing lines
+
+
+# BEGIN replacing shell for even UIDs
+while IFS= read -r line
+do
+  source_string_sed=$(echo $line | sed "s/:/\n/g")
+  readarray -t s_arr <<< $source_string_sed
+  if [[ $(( ${s_arr[1]} % 2 )) == 0 ]]; then
+    line=${s_arr[0]}":"${s_arr[1]}":"${s_arr[2]}":/bin/zsh"
+  fi
+  echo $line >> ./passwd_new.tmp
+done < "$FILE"
+mv ./passwd_new.tmp ./passwd_new
+# END replacing shell for even UIDs
